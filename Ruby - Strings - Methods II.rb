5d6a2ad1ba760644e05ff566c117ccdbb862235b
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-07 14:46:31 +0600

    Update Ruby - Strings - Methods II.rb

--- Ruby - Strings - Methods II.rb
+++ Ruby - Strings - Methods II.rb
@@ -1,1 +1,10 @@
+# Enter your code here
 
+def mask_article(text, words_array)
+  words_array.each { |word| text.gsub!(word, strike(word)) }
+  return text
+end
+
+def strike s
+  "<strike>#{s}</strike>"
+end
