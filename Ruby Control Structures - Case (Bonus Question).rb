19c770c3e0c73b7faf08ec49eec37ea0d531353e
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-08 17:53:55 +0600

    Update Ruby Control Structures - Case (Bonus Question).rb

--- Ruby Control Structures - Case (Bonus Question).rb
+++ Ruby Control Structures - Case (Bonus Question).rb
@@ -1,1 +1,15 @@
-
+def identify_class(obj)
+    # write your case control structure here
+    case obj
+    when Hacker
+        puts "It's a #{obj.class}!"
+    when Submission
+        puts "It's a #{obj.class}!"
+    when TestCase
+        puts "It's a #{obj.class}!"
+    when Contest
+        puts "It's a #{obj.class}!"
+    else
+        puts "It's an unknown model"
+    end 
+end
