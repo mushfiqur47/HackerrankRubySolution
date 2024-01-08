d7655e65a9a1c87f553920d3926ffab91f7801d2
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-08 18:00:32 +0600

    Update Ruby Array - Selection.rb

--- Ruby Array - Selection.rb
+++ Ruby Array - Selection.rb
@@ -23,15 +23,15 @@ end
 
 
 #1. `select_arr(arr)`
-   - This method uses the `select` method to return a new array containing only the elements from the input array `arr` that are odd (numbers not divisible by 2). It checks each element in the array using the condition `(a % 2 != 0)` where `%` is the modulo operator, and `a` represents each element in the array.
+  # - This method uses the `select` method to return a new array containing only the elements from the input array `arr` that are odd (numbers not divisible by 2). It checks each element in the array using the condition `(a % 2 != 0)` where `%` is the modulo operator, and `a` represents each element in the array.
 
 #2. `reject_arr(arr)`
-   - This method uses the `delete_if` method to remove elements from the input array `arr` that are divisible by 3. It iterates through each element and deletes it if the condition `(a % 3 == 0)` is satisfied.
+  # - This method uses the `delete_if` method to remove elements from the input array `arr` that are divisible by 3. It iterates through each element and deletes it if the condition `(a % 3 == 0)` is satisfied.
 
 #3. `delete_arr(arr)`
-   - This method uses the `delete_if` method to remove elements from the input array `arr` that are negative (less than 0). It iterates through each element and deletes it if the element is less than 0.
+  # - This method uses the `delete_if` method to remove elements from the input array `arr` that are negative (less than 0). It iterates through each element and deletes it if the element is less than 0.
 
 #4. `keep_arr(arr)`
-   - This method uses the `keep_if` method to retain elements in the input array `arr` that are non-negative (greater than or equal to 0). It iterates through each element and keeps it only if the condition `a >= 0` is met.
+ #  - This method uses the `keep_if` method to retain elements in the input array `arr` that are non-negative (greater than or equal to 0). It iterates through each element and keeps it only if the condition `a >= 0` is met.
 
 #Each method performs a specific filtering operation on the array based on different conditions and returns the modified array. It's important to note that `delete_if` modifies the array in place, while `select` and `keep_if` create a new array with the selected elements meeting the specified criteria.
