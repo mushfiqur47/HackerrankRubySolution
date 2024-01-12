e3d0617392b88b614bb8f5cc8d37d70961856d9a
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-12 16:42:23 +0600

    Update Ruby Hash - Addition, Deletion, Selection.rb

--- Ruby Hash - Addition, Deletion, Selection.rb
+++ Ruby Hash - Addition, Deletion, Selection.rb
@@ -1,1 +1,6 @@
 # Enter your code here.
+hackerrank= Hash.new
+hackerrank.default = 0
+hackerrank.store(543121, 100)
+hackerrank.keep_if {|key, value| key.is_a?Integer}
+hackerrank.delete_if {|key,value| key.even?}
