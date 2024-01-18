09bd784a2deb9d41e776e2e2b4afb8395ffc1972
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-18 16:27:53 +0600

    Update Ruby - Enumerable - collect.rb

--- Ruby - Enumerable - collect.rb
+++ Ruby - Enumerable - collect.rb
@@ -1,1 +1,10 @@
+def tranf(letra)
+  return letra unless letra.match?(/[A-Za-z]/)
 
+  offset = letra.upcase == letra ? 'A'.ord : 'a'.ord
+  ((letra.ord - offset + 13) % 26 + offset).chr
+end
+
+def rot13(secret_messages)
+  secret_messages.map { |message| message.each_char.map { |c| tranf(c) }.join }
+end
