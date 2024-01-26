d163d289c5bce67d6f1c70e1e2722661cb6c6a33
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-26 20:06:34 +0600

    Update Ruby - Enumerable - group_by.rb

--- Ruby - Enumerable - group_by.rb
+++ Ruby - Enumerable - group_by.rb
@@ -1,1 +1,10 @@
-
+def group_by_marks(marks, pass_marks)
+  # your code here
+    return marks.group_by do |k,v| 
+        if v>= pass_marks
+            next "Passed"
+        else
+            next "Failed"
+        end
+    end
+end
